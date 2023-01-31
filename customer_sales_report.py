7ba85376a6ad879ab6cb13b028b679a42a2f325b
Jiaze Lin <peter_lin2@baylor.edu>
2023-01-30 19:28:57 -0600

    1/30

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -8,13 +8,14 @@ out_file = open("salesreport.csv", "w", newline="")
 writer = csv.writer(out_file)
 writer.writerow(["CustomerID", "GrandTotal"])
 
-old_customer_id = None
+
+old_customer_id = ""
 grand_total = 0
 
 for row in csv_reader:
     customer_id = row[0]
     if old_customer_id != customer_id:
-        if old_customer_id is not None:
+        if old_customer_id != "":
             writer.writerow([old_customer_id, round(grand_total, 2)])
         old_customer_id = customer_id
         grand_total = 0
--- employee_bonus.py
+++ employee_bonus.py
@@ -10,6 +10,7 @@ header = ["ID", "Employee_Name", "Total_Pay"]
 print(header)
 
 for row in csv_reader:
-    print(row[0], row[1] + " " + row[2], int(row[3]) + int(row[3]) * float(row[4]))
+    total_pay = int(row[3]) + int(row[3]) * float(row[4])
+    print(row[0], row[1] + " " + row[2], round(total_pay, 2))
 
 in_file.close()
